57c75a40ec5f4cc2bb0d01e3eaa983b8d7294277
Mateus Adriano <106530497+syshydra@users.noreply.github.com>
2023-03-04 21:22:10 +0100

    add file
    dash complet

---
+++ assets/js/script.js
@@ -0,0 +1,123 @@
+/*=============== code drop image ===============*/
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+
+    dropZoneElement.addEventListener("click", (e) => {
+        inputElement.click();
+    });
+
+    inputElement.addEventListener("change", (e) => {
+        if (inputElement.files.length) {
+            updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+    });
+
+    dropZoneElement.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        dropZoneElement.classList.add("drop-zone--over");
+    });
+
+    ["dragleave", "dragend"].forEach((type) => {
+        dropZoneElement.addEventListener(type, (e) => {
+            dropZoneElement.classList.remove("drop-zone--over");
+        });
+    });
+
+    dropZoneElement.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+        }
+
+        dropZoneElement.classList.remove("drop-zone--over");
+    });
+});
+
+/**
+ * Updates the thumbnail on a drop zone element.
+ *
+ * @param {HTMLElement} dropZoneElement
+ * @param {File} file
+ */
+function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+        dropZoneElement.querySelector(".drop-zone__prompt").remove();
+    }
+
+    // First time - there is no thumbnail element, so lets create it
+    if (!thumbnailElement) {
+        thumbnailElement = document.createElement("div");
+        thumbnailElement.classList.add("drop-zone__thumb");
+        dropZoneElement.appendChild(thumbnailElement);
+    }
+
+    thumbnailElement.dataset.label = file.name;
+
+    // Show thumbnail for image files
+    if (file.type.startsWith("image/")) {
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+        };
+    } else {
+        thumbnailElement.style.backgroundImage = null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// olders
+$('#menu-btn').click(function (e) { 
+    e.preventDefault();
+    $('#sidebar').toggle('active');
+});
+
+$('#close-btn').click(function (e) { 
+    e.preventDefault();
+    $('#sidebar').toggle('active');
+});
+
+
+$('.btn-move-main').click(function (e) { 
+    $('.btn-move-main').toggle('active');  
+    $('.btn-move-right').toggle('active');
+});
+
+$('.btn-move-right').click(function (e) { 
+    $('.btn-move-main').toggle('active');    
+    $('.btn-move-right').toggle('active');
+});
+
+let list = document.querySelectorAll('.list');
+function activeLink() {
+	// body...
+	list.forEach((item) => 
+	item.classList.remove('active'));
+	this.classList.add('active');
+}
+
+list.forEach((item) => 
+	item.addEventListener('click',activeLink))
